60fddf3d2a1c22395b7351ab542a398a26834c16
Steffe Koerner <steffen.koerner@mytum.de>
2023-02-19 11:24:15 +0100

    Add more tests

--- git/bisect/test.cpp
+++ git/bisect/test.cpp
@@ -1,8 +1,12 @@
 #include <gtest/gtest.h>
 #include "bisect.h"
 
-TEST(SkipTest, DoesSkip) {
-
+TEST(sum, AdditionPositiveValues) {
   const int result = sum(3,4);
   EXPECT_EQ(result, 7);
+}
+
+TEST(sum, AdditionNegativeValues) {
+  const int result = sum(-3,-6);
+  EXPECT_EQ(result, -9);
 }
